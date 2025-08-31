ef6fbaaf339b5fcbe46ef9478b4c53a44905deff
Devendra Ahirwar <ahirwardev5416@gmail.com>
2025-08-31 10:15:42 +0530

    Update PyramidPattern.java
    create some changes

--- PyramidPattern.java
+++ PyramidPattern.java
@@ -1,21 +1,22 @@
 package Java-Pattern-Printing;
 
     import java.util.Scanner;
+
 public class PyramidPattern {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the value of n :");
     int n = sc.nextInt();
     for(int i=0; i<n; i++){
-        for(int j=0; j<n-i-
-        ; j++){
-            System.out.println("                            ");
+        //space
+        for(int j=0; j<n-i-1; j++){
+            System.out.println(" ");
         }
         for(int k=0; k<i; k++){
             System.out.print("* ");
 
         }
-        System.out.println(" ");
+        System.out.println(" ");  //new line
     }
 }
     
